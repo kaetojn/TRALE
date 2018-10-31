064810219b8a7af9c52e50fc2c02beee8046731c
Ndu Kaetochukwu <ndukaeto@teach.cs.toronto.edu>
2018-10-31 18:31:43 -0400

    Part 1 done

--- oneb.pl
+++ oneb.pl
@@ -1,125 +1,105 @@
 %Kaetochukwu Ndu, ndukaeto, 1000265955
 
-bot sub [type, number, cat].
-	type sub [verb, noun, pronoun].
-	    verb sub [].
-	    noun sub [].
-	tense sub [sg, pl].
-		sg sub [].
-		pl sub [].    	
-		pronoun sub [].
-	cat sub [s, np, vp, pp, n, v, p].
-		s sub [].
-		n sub [] intro [num:tense].
-			
-		np sub [] intro [head:n, phrase:type].
-
-		v sub [] intro [case:case, number:number, type:type].
-		vp sub [].
-		p sub [].
-		pp sub [].
-		det sub [].
+tense sub [sg, pl].
+	sg sub [].
+	pl sub [].
+type sub [noun, pronoun].
+	noun sub [].
+	pronoun sub [].
+cat sub [s, np, vp, pp, n, v, p, det].
+	s sub [].
+	n sub [] intro [tense:tense, type:type].
+	np sub [] intro [top:n].
+	v sub [] intro [vtense:tense].
+	vp sub [] intro [vpnp:np].
+	p sub [].
+	pp sub [] intro [ppnp:np].
+	det sub [].
 
 
 % Lexical Entries
-fido ---> (n, phrase:pronoun, num:pl)..
-biscuits ---> (n, phrase:noun, num:pl).
-feed ---> (v, phrase:verb, num:sg).
-feeds ---> (v, phrase:verb, num:pl).
+fido ---> (n, type:pronoun).
+biscuits ---> (n, tense:pl, type:noun).
+feed ---> (v, vtense:sg).
+feeds ---> (v, vtense:pl).
 the ---> det.
-dog ---> (n, phrase:noun, num:sg).
-puppies ---> (n, phrase:noun, num:pl).
+dog ---> (n, tense:sg, type:noun).
+puppies ---> (n, tense:pl, type:noun).
 with ---> p.
 
-
-S → NPsg VPsg
-S → NPpl VPpl
-VPsg → Vsg NP
-VPpl → Vpl NP
-PP → P NP
-
-NPsg → NPRP
-NPsg → Det Nsg
-
-
-NPsg → Det Nsg PP
-NPpl → Det Npl
-NPpl → Det Npl PP
-NPpl → Npl
-NPpl → Npl PP
-NP → NPsg
-NP → NPpl
-
-===>
-% Grammar 1: S -> NPsg VPsg | NPpl VPpl
-% Grammar 2: S -> NP VP
+%---------------------------------
+%Rule 1
+%Grammar2 | (Grammar 1)
+%S -> NP VP | NPsg VPsg | NPpl VPpl
 srule rule
 s
 ===>
-cat> (np, head:(case:nom)),
+cat> (np, top:(type:noun)),
+cat> (vp, vpnp:(top:(type:noun))).
+%---------------------------------
 
-
-
-
-cat> (np, head:(case:nom)),
-cat> (vp, obj_vp:(head:(case:acc))).
-
-
-
-
-% Grammar 1: VP -> V NP | V PROacc
-% Grammar 2: VP -> V NP
-vp_rule rule
-(vp, obj_vp:(head:(case:acc)))
+%---------------------------------
+%Rule 2
+%Grammar2 | (Grammar 1)
+%S -> V NP | (Vsg NP | Vpl NP)
+vprule rule
+vp
 ===>
 cat> v,
-cat> (np, head:(case:acc)).
+cat> (np, top:(type:noun)).
+%---------------------------------
 
-
-% Grammar 1: PP -> P NP | P PROacc
-% Grammar 2: PP -> P NP
-pp_rule rule
-(pp, obj_pp:(head:(case:acc)))
+%---------------------------------
+%Rule 3
+%Grammar2 | (Grammar 1)
+%P -> P NP | (P NPsg | P NPpl)
+pprule rule
+pp
 ===>
 cat> p,
-cat> (np, head:(case:acc)).
+cat> np.
+%---------------------------------
 
-
-% Grammar 1: NP -> Npl | PROnom | PROacc
-% Grammar 2: NP -> N
-np_rule rule
-(np, head:(number:plural, case:Case))
+%---------------------------------
+%Rule 4
+%Grammar2 | (Grammar 1)
+%NP -> N | (NPRP | Npl)
+np1 rule
+np
 ===>
-cat> (n, number:plural, case:Case).
-with ---> p.
+cat> n.
+%---------------------------------
 
-
-% Grammar 1: NP -> Det Nsg | Det Npl
-% Grammar 2: NP -> Det N
-np_rule rule
-(np, head:(type:noun))
+%---------------------------------
+%Rule 5
+%Grammar2 | (Grammar 1)
+%NP -> Det N | (Det Nsg | Det Npl) 
+np2 rule
+np
 ===>
 cat> det,
-cat> (n, type:noun).
+cat> (n, type:noun). 
+%---------------------------------
 
-
-% Grammar 1: NP -> Det Nsg PP | Det Npl PP
-% Grammar 2: NP -> Det N PP
-np_rule rule
-(np, head:(type:noun))
+%---------------------------------
+%Rule 6
+%Grammar2 | (Grammar 1)
+%NP -> Det N PP|(Det Nsg PP| Det Npl PP) 
+np3 rule
+np
 ===>
 cat> det,
 cat> (n, type:noun),
 cat> pp.
+%---------------------------------
 
-% Grammar 1: NP -> Npl PP
-% Grammar 2: NP -> N PP
-np_rule rule
-(np, head:(number:plural, type:noun))
+%---------------------------------
+%Rule 7
+%Grammar2 | (Grammar 1)
+%NP -> N PP| (Npl PP)
+np4 rule
+np
 ===>
-cat> (n, number:plural, type:noun),
+cat> (n, tense:pl, type:noun),
 cat> pp.
-
-
-
-
+%---------------------------------
